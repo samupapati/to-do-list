689631697ae9bb515ae5c1bf99edd1b0148b8fe3
samupapati <samuelpapati@gmail.com>
2023-04-15 23:28:56 -0300

    manipulação do input

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,21 +1,35 @@
 import 'boxicons';
+import { useState } from 'react';
 
-function Menu(props){
-    let valueInput = ''
-    
-    function saveInput(value){
-        valueInput = value
+function Menu({addTask, mouseOver, mouseOut}){
+    const [valueInput, setValueInput] = useState('')
+    function handleInput(value){
+        setValueInput(value)
     }
-    function handleInput(descTask){
-        props.handleTask(valueInput)
-        valueInput = ''
+    function handleSubmit(){
+        if(valueInput !== ''){
+            addTask(valueInput)
+            handleInput('')
+        }
     }
 
     return(
-        <div>
-            <input onChange={event => saveInput(event.target.value)}/>
-            <box-icon onClick={() => handleInput()} name='add-to-queue'></box-icon>
-
+        <div className='fundo' id='fundo-menu'>
+            <div className='over-fundo' id='menu'>
+                <h1>To Do List</h1>
+                <div className='container-input'>
+                    <input className='input' value={valueInput} onChange={event => handleInput(event.target.value)} placeholder='New task' autoFocus
+                        onKeyDown={(e) => {
+                            if(e.key === 'Enter'){
+                                handleSubmit()
+                            }
+                        }}
+                    />
+                    <box-icon onClick={() => handleSubmit()} className="btn" name='add-to-queue' color="var(--bg-btn)" 
+                        onMouseOver={(e) => mouseOver(e.target)} onMouseOut={(e) => mouseOut(e.target)}>
+                    </box-icon>
+                </div>
+            </div>
         </div>
     );
 }
